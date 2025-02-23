432c9aef39c6b181b78337e9b9271f21bc266eeb
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-23 15:11:28 +0000

    Initialisation of my backend app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Server backend
+import express from "express"
+
+// Initialer l'application
+const app = express()
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`My app is running on URL http://localhost:${PORT}`);
+})
