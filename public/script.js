7a65ce24344a6a07848557a128b860b08ff3eba4
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-24 07:13:45 +0000

    Add new user request

--- public/script.js
+++ public/script.js
@@ -7,11 +7,40 @@ async function displayUsers() {
 
     for (const user of users) {
         const li = document.createElement('li');
-        li.innerText = user.name;
+        li.innerText = `${user.id} - ${user.name}`;
         ulElement.appendChild(li);
     };
 };
 
+async function addUserName(name) {
+    const response = await fetch('/new-user', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name })
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+        console.log("User added:", result);
+        displayUsers();
+    } else {
+        console.error("Failed to add user");
+        throw new Error("Failed to add user");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     displayUsers();
+});
+
+const form = document.getElementById("user-form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name");
+    await addUserName(name);
+    form.reset();
 });
